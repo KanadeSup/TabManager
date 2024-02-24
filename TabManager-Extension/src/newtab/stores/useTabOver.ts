a456d8e20f98cfe8ad3875c9a390c4bcd2717846
Kanade <kanadesupreme@gmail.com>
2024-02-25 01:48:04 +0700

    refactor:  use Tab type instead TabItemData

--- TabManager-Extension/src/newtab/stores/useTabOver.ts
+++ TabManager-Extension/src/newtab/stores/useTabOver.ts
@@ -1,21 +1,15 @@
+import { Tab } from '@/types/Tab';
 import { create } from 'zustand'
-
-type TabItemData = {
-   id: string | undefined;
-   title: string | undefined;
-   url: string | undefined;
-   icon: string | undefined;
-}
 
 type TabOverStore = {
    dropOverId: string | null;
-   tabItemData: TabItemData | null;
+   tabItemData: Tab | null;
    setDropOverId: (id: string | null) => void;
-   setTabItemData: (data: TabItemData | null) => void;
+   setTabItemData: (data: Tab | null) => void;
 }
 export const useTabOverStore = create<TabOverStore>(set => ({
    dropOverId : null,
    tabItemData: null,
-   setDropOverId: (id: string | null) => set({dropOverId: id}),
-   setTabItemData: (data: TabItemData | null) => set({tabItemData: data})
+   setDropOverId: (id) => set({dropOverId: id}),
+   setTabItemData: (data) => set({tabItemData: data})
 }))
